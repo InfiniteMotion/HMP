f604ee7176745729aa07ea8b60aea095d83240b2
yuxi <wlybaiyjy@163.com>
2025-12-30 22:23:36 +0800

    chore(release): update version to 5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 33
         targetSdk = 36
         versionCode = 5
-        versionName = "5.2"
+        versionName = "5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
