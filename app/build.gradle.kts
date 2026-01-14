8f352d1d6cb61f47c4ef25beff67660bc811aa65
yuxi <wlybaijyj@163.com>
2026-01-14 20:19:44 +0800

    chore(release): update version to 5.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 33
         targetSdk = 36
         versionCode = 5
-        versionName = "5.3.1"
+        versionName = "5.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
