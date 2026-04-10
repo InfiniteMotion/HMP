4bb9c89cff00fc2b4fa3dd0b6703909c4aa9029c
WLYB2021 <WLYB2021@users.noreply.github.com>
2026-04-10 08:28:26 +0000

    feat: 主要技术栈升级调研
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- feature-ui/build.gradle.kts
+++ feature-ui/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+    kotlin("android") version "2.2.21"
 }
 
 android {
